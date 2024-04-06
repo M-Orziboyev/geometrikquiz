416254f79b8cf2aef372ce5edffdcff10864de9d
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2024-04-06 16:43:21 +0500

    frontend side

---
+++ js/quiz.js
@@ -0,0 +1,35 @@
+const quizArr = [
+  {
+    id: 1,
+    question: "",
+    options: {
+      A: "",
+      B: "",
+      C: "",
+      D: "",
+    },
+    answer: "B",
+  },
+  {
+    id: 2,
+    question: "",
+    options: {
+      A: "",
+      B: "",
+      C: "",
+      D: "",
+    },
+    answer: "C",
+  },
+  {
+    id: 3,
+    question: "",
+    options: {
+      A: "",
+      B: "",
+      C: "",
+      D: "",
+    },
+    answer: "A",
+  }
+];
